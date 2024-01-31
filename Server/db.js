fa38f19c023294f76d9fe50e42524241665fb7de
Luis_Gomez <luisalbertogomezbroca@gmail.com>
2024-01-31 10:41:05 -0500

    modificación de backend

--- Server/db.js
+++ Server/db.js
@@ -3,7 +3,7 @@ import {createPool} from 'mysql2/promise'
 export const pool = createPool({
     host: 'localhost',
     user: 'root',
-    password: '',
+    password: 'root',
     port: 3306,
     database: 'gestion_proyectos'
 })
--- Server/routes/tareas.routes.js
+++ Server/routes/tareas.routes.js
@@ -1,23 +1,3 @@
-<<<<<<< HEAD
-import { Router } from "express";
-import {
-    agregarTarea,
-    obtenerTarea,
-    obtenerTareas,
-    editarTarea,
-    eliminarTarea
-    
-} from '../controllers/tareas.controller.js'
-
-const router =  Router();
-router.post('/tareas', agregarTarea);
-router.get('/tareas/:id', obtenerTarea);
-router.get('/tareas', obtenerTareas);
-router.get('/tareas/:id', editarTarea);
-router.get('/tareas/:id', eliminarTarea);
-
-export default router
-=======
 import {Router} from 'express';
 import {
     agregarTarea,
@@ -36,4 +16,3 @@ router.put('/tareas/:id', editarTarea);
 router.delete('/tareas/:id', eliminarTarea);
 
 export default router;
->>>>>>> 0ea49ace5ddf1fbf95f4ffad26f613d58aef7856
